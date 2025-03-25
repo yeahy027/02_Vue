d856087110949dd627df57f5349388741a5c4c67
YeYoung Kim <yeahy027@naver.com>
2025-03-25 17:22:30 +0900

    20250325 기본과제

---
+++ 20250325/20250325_김예영_기본/test-vue-app/src/components/InputName.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="border: solid 1px gray; padding: 5px">
+    이름: <input type="text" v-model="name" />
+    <button @click="$emit('nameChanged', { name })">이벤트 발신</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputName',
+  emits: {
+    nameChanged: (e) => {
+        return e.name && typeof e.name === 'string' && e.name.trim().length >= 3 ? true : false;
+    }
+  }
+  data() {
+    return { name: '' };
+  },
+};
+</script>
