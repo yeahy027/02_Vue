500ddec734218ba7abce54f661f25348594aca5f
YeYoung Kim <yeahy027@naver.com>
2025-03-27 15:11:58 +0900

    20250327 기본과제

---
+++ 20250327/20250327_김예영_기본/calc-component-test/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Calc />
+    <hr />
+    <Calc2 />
+    <hr />
+    <Calc3 />
+    <hr />
+    <Calc4 />
+    <hr />
+    <Calc5 />
+  </div>
+</template>
+
+<script>
+import Calc from './components/Calc.vue';
+import Calc2 from './components/Calc2.vue';
+import Calc3 from './components/Calc3.vue';
+import Calc4 from './components/Calc4.vue';
+import Calc5 from './components/Calc5.vue';
+
+export default {
+  name: 'App',
+  components: { Calc, Calc2, Calc3, Calc4, Calc5 },
+};
+</script>
