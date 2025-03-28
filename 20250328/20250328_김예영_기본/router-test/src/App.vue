47defb4ae5b24f41818fa62b045401ab5c1167de
YeYoung Kim <yeahy027@naver.com>
2025-03-28 16:10:08 +0900

    20250328 기본과제

---
+++ 20250328/20250328_김예영_기본/router-test/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <!-- Header 컴포넌트 -->
+    <Header />
+
+    <!-- 주소에 따라 라우트된 컴포넌트가 보여질 자리 지정 -->
+    <!-- <router-view></router-view> -->
+    <RouterView />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components: { Header },
+};
+</script>
+
+<style>
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+
+.fa {
+  padding: 5px;
+  width: 30px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+}
+
+.fa-facebook {
+  background: #3b5998;
+  color: white;
+}
+
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+.container {
+  text-align: center;
+  margin-top: 10px;
+}
+</style>
---
+++ 20250328/20250328_김예영_기본/router-test/src/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card card-body">
+    <h2>About</h2>
+    <p>요청 경로 : {{ currentRoute.fullPath }}</p>
+  </div>
+</template>
+
+<script>
+import { useRoute } from 'vue-router';
+
+export default {
+  name: 'About',
+
+  setup() {
+    const currentRoute = useRoute();
+    return { currentRoute };
+  },
+};
+</script>
